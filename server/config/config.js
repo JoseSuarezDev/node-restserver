d693d507b19c1b16229f1a4c391d4ea12a916db6
Jose Suarez <josuasalaverria@gmail.com>
2021-01-26 11:13:00 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,20 @@
 // Puerto
 // ===================
 process.env.PORT = process.env.PORT || 3000;
+
+// ===================
+// Entorno
+// ===================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===================
+// Base de datos
+// ===================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://udemy:nsTbKaoXH3ugFp65@cluster0.x2t5e.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,38 +14,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario/:id', function (req, res) {
-    res.json('get Usuario')
-});
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(resp => {
 
-app.post('/usuario', function (req, res) {
+    console.log('Base de datos ONLINE');
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario/:id', function (req, res) {
-    res.json('delete Usuario')
-});
+}).catch(err => { throw err });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
