dd6224acb842fb8eb4dab7c639571ceea1ebcc88
Jose Suarez <josuasalaverria@gmail.com>
2021-01-26 11:41:04 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://udemy:nsTbKaoXH3ugFp65@cluster0.x2t5e.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
